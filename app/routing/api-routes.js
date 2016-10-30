864017a22236a77006df821ccc7715508446aea1
ANDRES SAENZ <SAENZA76@YAHOO.COM>
2016-10-29 23:05:41 -0500

    latest commit, app still not working yet

---
+++ app/routing/api-routes.js
@@ -0,0 +1,15 @@
+var path = require('path');
+var bodyParser = require('body-parser');
+var friendData = require('../data/friends.js');
+
+module.exports = function(app) {
+
+	app.get("/api/friends", function(req,res) {
+		res.json(friendData);
+	});
+
+     app.post("/api/friends", function (req, res){
+          friendData.push(req.body);
+          res.json(friendData);
+     });
+};
